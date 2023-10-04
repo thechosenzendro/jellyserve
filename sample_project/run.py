7820bb861f0239d8900e7e5fae84620064b0787c
TheChosenZendro <the.chosen.zendro@gmail.com>
2023-10-04 21:35:31 +0200

    idk what am doing

--- sample_project/run.py
+++ sample_project/run.py
@@ -1,5 +1,4 @@
 from app import app
-import uvicorn
 import routes
 import messages
 
--- src/jellyserve/_generator.py
+++ src/jellyserve/_generator.py
@@ -1,19 +1,41 @@
 from .__init__ import JellyServe
-
+import os
+import time
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from ._config import config
+import subprocess
 class Generator:
     def __init__(self, app: JellyServe) -> None:
         self.app = app
     
     def start(self):
-        import os
-        import time
-        from watchdog.observers import Observer
-        from watchdog.events import FileSystemEventHandler
-        print(self.app.used_templates)
-        absolute_template_paths = {os.path.abspath(path) for path in self.app.used_templates}
-        print(absolute_template_paths)
+        template_paths = {os.path.abspath(path) for path in self.app.used_templates}
+        runtime_path = config.get("server/runtime_path")
+        generate_component()
+        if not os.path.exists(runtime_path):
+             os.mkdir(runtime_path)
 
-        class Handler(FileSystemEventHandler):
+        observer = Observer()
+
+        for path in template_paths:
+            handler = Handler()
+            observer.schedule(handler, path, recursive=False)
+
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print("Shutting down generator...")
+            if os.path.exists(runtime_path):
+                 shutil.rmtree(runtime_path)
+            observer.stop()
+        observer.join()
+
+
+class Handler(FileSystemEventHandler):
             def on_modified(self, event):
                 print(f"File {event.src_path} modified.")
 
@@ -23,17 +45,10 @@ class Generator:
             def on_deleted(self, event):
                 print(f"File {event.src_path} deleted.")
 
-        observer = Observer()
 
-        for path in absolute_template_paths:
-            handler = Handler()
-            observer.schedule(handler, path, recursive=False)
-
-        observer.start()
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            print("Shutting down svelte generator...")
-            observer.stop()
-        observer.join()
+def generate_component():
+    code = '''const { rollup } = require("rollup")'''
+    cmd = ["cd", config.get("templates/frontend_path"), "&&", "node", "--experimental-modules", "-e", code]
+    print(" ".join(cmd))
+    res = subprocess.check_output(cmd)
+    print(res.decode())
